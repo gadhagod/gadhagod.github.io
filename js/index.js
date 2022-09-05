5b87d34fff1be911bd8f8fd26bdfba76b3353708
gadhagod <69025547+gadhagod@users.noreply.github.com>
2022-09-05 22:43:30 +0000

    Update data.json
    Deploy

---
+++ js/index.js
@@ -0,0 +1,3 @@
+if(document.title === "Aarav Borthakur" && window.innerWidth < 1029) {
+    alert("This website is still in development, and currently does not support the size of your device/window as its to small.  You can still browse the site but images will be sized incorrectly. Sorry!")
+}
